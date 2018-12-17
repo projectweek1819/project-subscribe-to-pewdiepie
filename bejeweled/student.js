79fe9ebdce510edfb34ed79376d6c08fae8f515b
JonasBerx <43341402+JonasBerx@users.noreply.github.com>
2018-12-17 14:48:06 +0100

    Update 1.2
    Begin isInside

--- bejeweled/student.js
+++ bejeweled/student.js
@@ -2,7 +2,21 @@
 function width(grid) {
     return grid[0].length;
 }
+
 function height(grid) {
     return grid.length;
 }
 
+function isInside(grid, position) {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === position) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
+    }
+}
