0386cc26d9c82e4059cf8ebdda0e02776a6beaa5
JonasBerx <43341402+JonasBerx@users.noreply.github.com>
2018-12-17 21:52:34 +0100

    Update 1.5
    Vertical en Horizontal done

--- bejeweled/student.js
+++ bejeweled/student.js
@@ -19,15 +19,51 @@ function isInside(grid, position) {
 }
 
 function isInside(grid, position) {
-    const { x, y } = position;
+    const {x, y} = position;
     return 0 <= x && x < width(grid) && 0 <= y && y < height(grid);
 }
+
 function swap(grid,p,q) {
     const temp = grid[p.y][p.x];
     grid[p.y][p.x] = grid[q.y][q.x];
     grid[q.y][q.x] = temp;
 
 }
+
+function horizontalChainAt(grid, position) {
+    const { x, y } = position;
+    let i;
+    for (i = 1; x + i < width(grid); ++i) {
+        if (grid[y][x + i] !== grid[y][x]) {
+            break;
+        }
+    }
+    let j;
+    for (j = 1; x - j >= 0; ++j) {
+        if (grid[y][x - j] !== grid[y][x]) {
+            break;
+        }
+    }
+    return i + j - 1;
+}
+
+function verticalChainAt(grid, position) {
+    const {x, y} = position;
+    let i;
+    for (i = 1;y + i < height(grid); ++i) {
+        if (grid[y+ i][x] !== grid[y][x]) {
+            break;
+        }
+    }
+    let j;
+    for (j = 1; y - j >=0; ++j) {
+        if (grid[y-j][x] !== grid[y][x]) {
+            break;
+        }
+    }
+    return i + j - 1 ;
+}
+
 
 
 
