63cf4fa450c82e7d4ec728f310062aa5bf056d02
JonasBerx <43341402+JonasBerx@users.noreply.github.com>
2018-12-18 12:05:27 +0100

    Update 2.3
    Collapse almost done

--- bejeweled/student.js
+++ bejeweled/student.js
@@ -64,6 +64,57 @@ function verticalChainAt(grid, position) {
     return i + j - 1 ;
 }
 
+function removeChains(grid) {
+    const positions = [];
+    const result = {};
+    const w = width(grid);
+    const h = height(grid);
+    for (let y = 0; y !== h; ++y) {
+        let x = 0;
+        while (x < w) {
+            const n = horizontalChainAt(grid, {x, y});
+            if (n > 2) {
+                for (let i = 0; i !== n; ++i) {
+                    positions.push({x: x + i, y});
+                }
+            }
+            x += n;
+        }
+    }
+    for (let x = 0; x !== w; ++x) {
+        let y = 0;
+        while (y < h) {
+            const n = verticalChainAt(grid, {x, y});
+            if (n > 2) {
+                for (let i = 0; i !== n; ++i) {
+                    positions.push({x, y: y + i});
+                }
+            }
+            y += n;
+        }
+    }
+    for (const position of positions) {
+        const {x, y} = position;
+        const color = grid[y][x];
+        result[color] = (result[color] || 0) + 1;
+    }
+    for (const {x, y} of positions) {
+        grid[y][x] = '';
+    }
+    return result;
+}
 
+function collapse(grid) {
+    let collapsed = false;
+    for (let x = 0; x < width(grid)-1; x++) {
+
+        for (let y = height(grid)-1; y > 0; y--) {
+            if (grid[y][x] == "") {
+                
+            }
+        }
+
+    }
+}
 
 
